0ffeeaf5a71d4fc744856b9d1567de41cfb5fb1b
Michele Volpato <mvolpato@users.noreply.github.com>
2020-11-16 21:44:47 +0100

    Day 19 solution

---
+++ VolumeConverter/ContentView.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  VolumeConverter
+//
+//  Created by Michele Volpato on 16/11/2020.
+//
+
+import SwiftUI
+
+enum ConversionUnit: String, CaseIterable {
+    case milliliters, liters, cups, pints, gallons
+
+    var asMilliliters: Double {
+        switch self {
+        case .milliliters:
+            return 1
+        case .liters:
+            return 1000
+        case .cups:
+            return 284.131
+        case .pints:
+            return 568.261
+        case .gallons:
+            return 4546.0964008947
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var amountToConvert = ""
+    @State private var fromUnitIndex = 0
+    @State private var toUnitIndex = 0
+
+    private let units = ConversionUnit.allCases
+
+    private var convertedAmount: Double {
+        let doubleToConvert = Double(amountToConvert) ?? 0
+        let millilitersToConvert = units[fromUnitIndex].asMilliliters * doubleToConvert
+        let toUnitAsMilliliters = units[toUnitIndex].asMilliliters
+
+        return millilitersToConvert / toUnitAsMilliliters
+    }
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount to convert", text: $amountToConvert)
+                        .keyboardType(.decimalPad)
+                    Picker("From", selection: $fromUnitIndex) {
+                        ForEach(0..<units.count) {
+                            Text(units[$0].rawValue)
+                        }
+                    }
+                    Picker("To", selection: $toUnitIndex) {
+                        ForEach(0..<units.count) {
+                            Text(units[$0].rawValue)
+                        }
+                    }
+                }
+
+                Section(header: Text("\(amountToConvert) \(units[fromUnitIndex].rawValue) in \(units[toUnitIndex].rawValue):")) {
+                    Text("\(convertedAmount, specifier: "%.4f")")
+                }
+            }
+            .navigationBarTitle("Volume conversion")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VolumeConverter/VolumeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeConverterApp.swift
+//  VolumeConverter
+//
+//  Created by Michele Volpato on 16/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct VolumeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
